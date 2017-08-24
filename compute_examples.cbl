9e125c1f674750d83aeed423a74254b3b5b1b77c
Tom Gonzalez <tomg910@gmail.com>
2017-08-24 19:33:29 -0400

    Added more arithmetic examples
    Added more arithmetic examples

--- compute_examples.cbl
+++ compute_examples.cbl
@@ -1,11 +1,11 @@
       ******************************************************************
       * Author:  Tom Gonzalez
       * Date:  08/07/2014
-      * Purpose:  Give examples of the DIVIDE statement.
+      * Purpose:  Give examples of the COMPUTE statement.
       * Tectonics: cobc
       ******************************************************************
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. DIVIDE-EXAMPLES.
+       PROGRAM-ID. COMPUTE-EXAMPLES.
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
@@ -13,20 +13,17 @@
        01 NUM2         PIC 9(5)V9(5) VALUE 5.
        01 NUM3         PIC 9(5)V9(5) VALUE 6.
        01 NUM4         PIC 9(5)V9(5) VALUE 2.
-       01 NUM5         PIC 9(5)V9(5) VALUE 9.
-
-       01 NUM6         PIC 9(5)      VALUE 34.
-       01 NUM7         PIC 9(5)      VALUE 5.
-       01 Q            PIC 9(5).
-       01 R            PIC 9(5).
+       01 NUM5         PIC 9(5)V9(5).
+       01 NUM6         PIC 9(5)V9(3).
 
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
+
             DISPLAY "Example 1"
             DISPLAY "NUM1: ", NUM1, " NUM2: ", NUM2, " NUM3: ", NUM3,
                " NUM4: ", NUM4, " NUM5: ", NUM5
-            DIVIDE 8 INTO NUM1
-            DISPLAY "DIVIDE 8 INTO NUM1"
+            COMPUTE NUM5 = 2 * NUM1 + NUM2
+            DISPLAY "COMPUTE NUM5 = 2 * NUM1 + NUM2"
             DISPLAY "NUM1: ", NUM1, " NUM2: ", NUM2, " NUM3: ", NUM3,
                " NUM4: ", NUM4, " NUM5: ", NUM5
             DISPLAY " "
@@ -40,8 +37,8 @@
             DISPLAY "Example 2"
             DISPLAY "NUM1: ", NUM1, " NUM2: ", NUM2, " NUM3: ", NUM3,
                " NUM4: ", NUM4, " NUM5: ", NUM5
-            DIVIDE NUM2 INTO NUM3
-            DISPLAY "DIVIDE NUM2 INTO NUM3"
+            DISPLAY "COMPUTE NUM5 = 2 * NUM1 - 4 * NUM3 * NUM2"
+            DISPLAY "COMPUTE NUM5 = 2 * NUM1 - 4 * NUM3 * NUM2"
             DISPLAY "NUM1: ", NUM1, " NUM2: ", NUM2, " NUM3: ", NUM3,
                " NUM4: ", NUM4, " NUM5: ", NUM5
             DISPLAY " "
@@ -55,8 +52,8 @@
             DISPLAY "Example 3"
             DISPLAY "NUM1: ", NUM1, " NUM2: ", NUM2, " NUM3: ", NUM3,
                " NUM4: ", NUM4, " NUM5: ", NUM5
-            DIVIDE NUM4 INTO NUM3 GIVING NUM5
-            DISPLAY "DIVIDE NUM4 INTO NUM3 GIVING NUM5"
+            COMPUTE NUM5 = NUM3**2 + NUM4**2
+            DISPLAY "COMPUTE NUM5 = NUM3**2 + NUM4**2"
             DISPLAY "NUM1: ", NUM1, " NUM2: ", NUM2, " NUM3: ", NUM3,
                " NUM4: ", NUM4, " NUM5: ", NUM5
             DISPLAY " "
@@ -70,8 +67,8 @@
             DISPLAY "Example 4"
             DISPLAY "NUM1: ", NUM1, " NUM2: ", NUM2, " NUM3: ", NUM3,
                " NUM4: ", NUM4, " NUM5: ", NUM5
-            DIVIDE 3 INTO NUM1, NUM3
-            DISPLAY "DIVIDE 3 INTO NUM1, NUM3"
+            COMPUTE NUM5 = NUM1 + NUM2 * NUM3
+            DISPLAY "COMPUTE NUM5 = NUM1 + NUM2 * NUM3"
             DISPLAY "NUM1: ", NUM1, " NUM2: ", NUM2, " NUM3: ", NUM3,
                " NUM4: ", NUM4, " NUM5: ", NUM5
             DISPLAY " "
@@ -85,13 +82,13 @@
             DISPLAY "Example 5"
             DISPLAY "NUM1: ", NUM1, " NUM2: ", NUM2, " NUM3: ", NUM3,
                " NUM4: ", NUM4, " NUM5: ", NUM5
-            DIVIDE NUM2 INTO NUM3 GIVING NUM4, NUM5
-            DISPLAY "DIVIDE NUM2 BY NUM3 GIVING NUM4, NUM5"
+            COMPUTE NUM5 = ( NUM1 + NUM2 ) * NUM3
+            DISPLAY "COMPUTE NUM5 = ( NUM1 + NUM2 ) * NUM3"
             DISPLAY "NUM1: ", NUM1, " NUM2: ", NUM2, " NUM3: ", NUM3,
                " NUM4: ", NUM4, " NUM5: ", NUM5
             DISPLAY " "
 
-            MOVE 7 TO NUM1
+            MOVE 8 TO NUM1
             MOVE 5 TO NUM2
             MOVE 6 TO NUM3
             MOVE 2 TO NUM4
@@ -99,37 +96,14 @@
 
             DISPLAY "Example 6"
             DISPLAY "NUM1: ", NUM1, " NUM2: ", NUM2, " NUM3: ", NUM3,
-               " NUM4: ", NUM4, " NUM5: ", NUM5
-            DIVIDE NUM5 BY NUM4 GIVING NUM1, NUM5
-            DISPLAY "DIVIDE NUM5 BY NUM4 GIVING NUM1, NUM5"
+               " NUM4: ", NUM4, " NUM5: ", NUM5, " NUM6: ", NUM6
+            COMPUTE NUM5 = NUM1 / 3
+            DISPLAY "COMPUTE NUM5 = NUM1 / 3"
+            COMPUTE NUM6 ROUNDED = NUM1 / 3
+            DISPLAY "COMPUTE NUM6 ROUNDED = NUM1 / 3"
             DISPLAY "NUM1: ", NUM1, " NUM2: ", NUM2, " NUM3: ", NUM3,
-               " NUM4: ", NUM4, " NUM5: ", NUM5
-            DISPLAY " "
-
-      * Need to have integer PIC clauses to get remainders.
-
-            DISPLAY "Example 6"
-            DISPLAY "NUM6: ", NUM6, " NUM7: ", NUM7, " Q: ", Q,
-               " R: ", R
-            DIVIDE NUM6 BY NUM7 GIVING Q REMAINDER R
-            DISPLAY "DIVIDE NUM7 INTO NUM6 GIVING Q REMAINDER R"
-            DISPLAY "NUM6: ", NUM6, " NUM7: ", NUM7, " Q: ", Q,
-               " R: ", R
-            DISPLAY " "
-
-            MOVE 34 TO NUM6
-            MOVE 5 TO NUM7
-            MOVE 0 TO Q
-            MOVE 0 TO R
-
-            DISPLAY "Example 7"
-            DISPLAY "NUM6: ", NUM6, " NUM7: ", NUM7, " Q: ", Q,
-               " R: ", R
-            DIVIDE NUM7 INTO NUM6 GIVING Q REMAINDER R
-            DISPLAY "DIVIDE NUM7 INTO NUM6 GIVING Q REMAINDER R"
-            DISPLAY "NUM6: ", NUM6, " NUM7: ", NUM7, " Q: ", Q,
-               " R: ", R
+               " NUM4: ", NUM4, " NUM5: ", NUM5, " NUM6: ", NUM6
             DISPLAY " "
 
             STOP RUN.
-       END PROGRAM DIVIDE-EXAMPLES.
+       END PROGRAM COMPUTE-EXAMPLES.
